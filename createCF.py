344a0dd86bff40dbc79630f05381f84216caf3e0
Janos Potecki <janos.potecki.15@ucl.ac.uk>
2017-11-26 14:07:14 +0000

    Change codebuild spec file using `Sub` and `Join`

--- createCF.py
+++ createCF.py
@@ -142,7 +142,7 @@ def getTemplateFromFolder(path: str, stage: str) -> Template:
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
-  parser.add_argument("-p", "--path", help="Path of the folder with the source-code of the aws lambda functions")
+  parser.add_argument("-p", "--path", help="Path of the folder with the source-code of the aws lambda functions", required = True)
   parser.add_argument("--stage", help="Name of the stage", type = str, required = True)
   parser.add_argument("--stack", help="Name of the stack", type = str, required = True)
   args = parser.parse_args()
